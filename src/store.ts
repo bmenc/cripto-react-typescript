4180881eac6f81c8aec204c84deed4c841c0096a
bmenc <bmenchaca.cca@gmail.com>
2026-02-28 17:54:24 -0600

    289. Validando la respuesta de la API con Zod

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,15 @@
 import { create } from 'zustand'
 import axios from 'axios'
+import { CryptoCurrenciesResponseSchema, CryptoCurrencyResponseSchema } from './schema/crypto-schema'
 
 async function getCryptos() {
-  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit20&tsym=USD'
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   const { data: { Data } } = await axios(url)
   console.log(Data);
+
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+  console.log(result);
+
 
 }
 
