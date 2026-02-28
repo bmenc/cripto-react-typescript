7fff5856a924f254f142ee2729e94b0952b61837
bmenc <bmenchaca.cca@gmail.com>
2026-02-28 17:27:06 -0600

    288. Obteniendo las Criptomonedas en Zustand

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,16 @@
 import { create } from 'zustand'
+import axios from 'axios'
+
+async function getCryptos() {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit20&tsym=USD'
+  const { data: { Data } } = await axios(url)
+  console.log(Data);
+
+}
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
     console.log('Desde FetchCryptos');
-
+    getCryptos()
   }
 }))
