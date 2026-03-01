e0b20717e9355ecbb4817ec95ec1cc405ba465fe
bmenc <bmenchaca.cca@gmail.com>
2026-02-28 18:32:09 -0600

    290. Finalizando la función que obtiene las Criptos

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,28 @@
 import { create } from 'zustand'
 import axios from 'axios'
-import { CryptoCurrenciesResponseSchema, CryptoCurrencyResponseSchema } from './schema/crypto-schema'
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema'
+import type { CryptoCurrency } from './types'
+
+type CryptoStore = {
+  cryptocurrencies: CryptoCurrency[]
+  fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos() {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   const { data: { Data } } = await axios(url)
-  console.log(Data);
-
   const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-  console.log(result);
-
-
+  if (result.success) {
+    return result.data
+  }
 }
 
-export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {
-    console.log('Desde FetchCryptos');
-    getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+  cryptocurrencies: [],
+  fetchCryptos: async () => {
+    const cryptocurrencies = await getCryptos()
+    set(() => ({
+      cryptocurrencies
+    }))
   }
 }))
